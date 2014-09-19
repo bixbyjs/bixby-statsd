20de86cc2ff7e3cbea8c06ff5b5e5d5dcd1e52ad
Jared Hanson <jaredhanson@gmail.com>
2014-09-19 15:03:59 -0700

    Fix auto-generated test case.

--- test/package.test.js
+++ test/package.test.js
@@ -5,7 +5,7 @@ var pkg = require('..');
 describe('bixby-statsd', function() {
   
   it('should export object', function() {
-    expect(pkg).to.be.an('object');
+    expect(pkg).to.be.an('function');
   });
   
 });
